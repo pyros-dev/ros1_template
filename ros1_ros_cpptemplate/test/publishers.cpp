5fa14d0187a74f0f5259be847ab399b4e4c8bac5
Alexander Reimann <A.Reimann@yujinrobot.com>
2017-08-23 14:51:42 +0900

    Working publishers rostests

---
+++ ros1_ros_cpptemplate/test/publishers.cpp
@@ -0,0 +1,68 @@
+#include <gtest/gtest.h>
+
+#include <ros/master.h>
+#include <std_msgs/Int32.h>
+#include <ros/duration.h>
+#include <ros/subscriber.h>
+#include <ros/node_handle.h>
+
+#include <atomic>
+#include <iostream>
+
+class Int32CallbackTest
+{
+public:
+  Int32CallbackTest(const std::string& topic)
+  {
+    got_msg_ = false;
+    ros::NodeHandle node_handle;
+    int queue_size = 1;
+    ros::Subscriber subscriber = node_handle.subscribe(topic, queue_size, &Int32CallbackTest::callback, this);
+
+    while (subscriber.getNumPublishers() == 0 && ros::ok())
+    {
+      ros::Duration(0.001).sleep();
+    }
+
+    while (!got_msg_.load() && ros::ok())
+    {
+      ros::spinOnce();
+      ros::Duration(0.001).sleep();
+    }
+  }
+
+  ~Int32CallbackTest()
+  {
+    EXPECT_TRUE(got_msg_.load());
+  }
+
+  void callback(const std_msgs::Int32Ptr& number)
+  {
+    got_msg_ = true;
+  }
+
+private:
+  std::atomic<bool> got_msg_;
+};
+
+TEST(Publishers, internal)
+{
+  Int32CallbackTest("fibonacci_publisher_internal");
+}
+
+TEST(Publishers, other)
+{
+  Int32CallbackTest("fibonacci_publisher_other");
+}
+
+// Run with "catkin_make run_tests_ros1_cpptemplate"
+int main(int argc, char **argv)
+{
+  testing::InitGoogleTest(&argc, argv);
+  int time_seed = static_cast<int>(time(0));
+  srand(time_seed);
+
+  ros::init(argc, argv, "publishers");
+
+  return RUN_ALL_TESTS();
+}
